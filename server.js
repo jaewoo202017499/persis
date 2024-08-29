3fe6a219011e8f85d6858fb88d8aa66c9e48ccac
jaewoo <hahahoho3797@naver.com>
2024-08-30 00:29:36 +0900

    채팅삭제완료

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const server = createServer(app)
 const io = new Server(server)
 
 let db
+let chatMessage = {}
+let deleteTime = 5
 const url = process.env.DB_URL
 new MongoClient(url).connect().then((client) => {
     console.log('DB연결성공')
@@ -32,9 +34,14 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 })
+app.get('/list', async (요청, 응답) => {
+    let chatroom = await db.collection('chatroom').find().toArray()
+    응답.render('chatList.ejs', { userID: 요청.query.id, room: chatroom })
+})
 
 app.post('/list', async (요청, 응답) => {
 
+    // 계정 새로 만든 경우 방 만들기
     let myRoom = await db.collection('chatroom').findOne({ creatUserID: 요청.body.id })
     if ((myRoom)) {
         console.log('존재함')
@@ -42,16 +49,14 @@ app.post('/list', async (요청, 응답) => {
         await db.collection('user').insertOne({
             id: 요청.body.id
         })
-
         await db.collection('chatroom').insertOne({
             creatUserID: 요청.body.id,
             userID: [요청.body.id],
-            userNum: 1,
             date: new Date()
         })
         console.log('계정 새로 만들었음')
     }
-
+    // 전체 채팅방 공유하기
     let chatroom = await db.collection('chatroom').find().toArray()
     응답.render('chatList.ejs', { userID: 요청.body.id, room: chatroom })
 })
@@ -59,6 +64,7 @@ app.post('/list', async (요청, 응답) => {
 app.post('/room', async (요청, 응답) => {
     let currentRoom = await db.collection('chatroom')
         .findOne({ creatUserID: 요청.body.creatUserID })
+    // 랜덤으로 돌릴때 내가 속하지 않은곳으로 이동하기
     if (currentRoom.userID.includes(요청.body.userID)) {
     } else {
         let user = currentRoom.userID
@@ -67,19 +73,18 @@ app.post('/room', async (요청, 응답) => {
             { _id: currentRoom._id },
             {
                 $set: { userID: user },
-                $inc: { userNum: 1 }
             }
         )
         currentRoom = await db.collection('chatroom')
             .findOne({ userID: 요청.body.userID })
     }
-    roomName = JSON.parse(JSON.stringify(currentRoom._id))
-    let messages = await db.collection(roomName).find().toArray()
-
+    // 이전 메시지 불러오기
+    let roomID = currentRoom._id.toString()
+    let preMessage = chatMessage[roomID]
     응답.render('chatRoom.ejs', {
         room: currentRoom,
         userID: 요청.body.userID,
-        chat: messages
+        chat: preMessage ? preMessage : []
     })
 
 })
@@ -97,33 +102,31 @@ app.post('/room/:next', async (요청, 응답) => {
     ]).toArray()
     currentRoom = currentRoom[0]
 
-    if (currentRoom.userID.includes(요청.body.userID)) {
-    } else {
-        let user = currentRoom.userID
-        user.push(요청.body.userID)
-        await db.collection('chatroom').updateOne(
-            { _id: currentRoom._id },
-            { $set: { userID: user }, $inc: { userNum: 1 } }
-        )
-        currentRoom = await db.collection('chatroom')
-            .findOne({ userID: 요청.body.userID })
-    }
-    roomName = JSON.parse(JSON.stringify(currentRoom._id))
-    let messages = await db.collection(roomName).find().toArray()
+    // 랜덤채팅방 돌리기
+    let user = currentRoom.userID
+    user.push(요청.body.userID)
+    await db.collection('chatroom').updateOne(
+        { _id: currentRoom._id },
+        { $set: { userID: user } })
+    currentRoom = await db.collection('chatroom')
+        .findOne({ userID: 요청.body.userID })
+
+    // 이전 메시지 불러오기
+    let roomID = currentRoom._id.toString()
+    let preMessage = chatMessage[roomID]
 
     응답.render('chatRoom.ejs', {
         room: currentRoom,
         userID: 요청.body.userID,
-        chat: messages
+        chat: preMessage ? preMessage : []
     })
 })
 
 app.post('/page-leave', async (요청, 응답) => {
     db.collection('chatroom').updateOne(
-        { _id: new ObjectId(요청.body.roomID) },
+        { _id: new ObjectId(요청.query.roomID) },
         {
-            $pull: { userID: 요청.body.userID },
-            $inc: { userNum: -1 }
+            $pull: { userID: 요청.query.userID },
         }
     );
     응답.sendStatus(200); // 클라이언트에 응답
@@ -133,17 +136,42 @@ io.on('connection', (socket) => {
 
     socket.on('ask-join', (roomID) => {
         socket.join(roomID)
+        if (!chatMessage[roomID]) {
+            chatMessage[roomID] = []
+        }
         room = io.sockets.adapter.rooms.get(roomID);
-        io.emit('room-size', room.size)
+        io.to(roomID).emit('room-size', room.size)
     })
 
     socket.on('message', async (data) => {
+        // 서버에저장
+        let currentTime = new Date()
+        let message = { userID: data.user, text: data.text, time: currentTime }
+        chatMessage[data.roomID].push(message)
+        console.log(chatMessage[data.roomID])
+
+        // DB저장
         await db.collection(data.roomID).insertOne({
             writer: data.user,
             message: data.msg,
-            time: new Date()
+            time: currentTime
         })
-        io.to(data.roomID).emit('msg', { message: data.msg, user: data.user });
+        io.to(data.roomID).emit('msg', { message: data.text, user: data.user });
+
+        // 채팅지우기
+        setTimeout(async () => {
+            console.log(data.roomID)
+            chatMessage[data.roomID].shift()
+            // chatMessage[data.roomID].
+            io.to(data.roomID).emit('delete-message', { message: data.text, user: data.user })
+        }, 1000 * deleteTime); // 60000 밀리초 = 1분
+    })
+
+    socket.on('leave-page', (roomID) => {
+        console.log(roomID)
+        room = io.sockets.adapter.rooms.get(roomID);
+        console.log(room.size - 1)
+        io.to(roomID).emit('room-size', room.size - 1)
     })
 
 }) 
